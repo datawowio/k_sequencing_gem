d904ce0c8eb41680bd9093102086bd48ab973863
aun <aun.sira@gmail.com>
2018-03-26 14:15:48 +0700

    Add `successful?` to the response Client can be able to check whether request is success by checking `response.successful?` to the Response object

--- test/k_sequencing/client_response_test.rb
+++ test/k_sequencing/client_response_test.rb
@@ -3,9 +3,17 @@ require 'test_helper'
 module KSequencing
   class ResponseTest < Minitest::Test
     def test_new_success_response
-      response = Response.new('response', true, 200, 'success', 'meta', 5)
+      response = Response.new('response', 200, 'success', 'meta', 5)
       refute_nil(response)
       refute_nil(response.data)
+      assert_equal(true, response.successful?)
+    end
+
+    def test_failure_response
+      response = Response.new('response', 500, 'failure', 'meta')
+      refute_nil(response)
+      refute_nil(response.data)
+      assert_equal(false, response.successful?)
     end
   end
 end
